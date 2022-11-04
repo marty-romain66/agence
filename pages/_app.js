bd1976b23061a0621523ddb06f0e6345ca5af514
marty-romain66 <martyromain.dev@gmail.com>
2022-11-04 16:23:20 +0100

    okkk

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,38 @@ import Layout from '../components/Layout'
 import '../styles/globals.css'
 import "../styles/index.scss"
 import Script from 'next/script'
+import AnimatedCursor from "react-animated-cursor";
 
 function MyApp({ Component, pageProps }) {
   return(
-  <><Layout>
+
+  <>
+    <AnimatedCursor
+        innerSize={8}
+        outerSize={50}
+        color="193, 11, 111"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={1.8}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+          "h2",
+          "p",
+        ]}
+      />
+  
+  
+  <Layout>
 
       <Component {...pageProps} />
 
