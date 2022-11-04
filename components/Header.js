9ad6ff22f4c8888d49e15965da642a8d86d12b5a
marty-romain66 <martyromain.dev@gmail.com>
2022-11-04 01:52:03 +0100

    ok

--- components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-  return (
-    <div>je suis le Header</div>
-  )
-}
--- components/Layout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Footer from './Footer'
-import Header from './Header'
-
-export default function Layout() {
-  return (
-    <><Header /><Footer /></>
-  )
-}
